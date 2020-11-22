8ff34b7bf2de1e18cbd5670047c44cdad784d38f
mohamedthariq <mohamedthariq1997@gmail.com>
2020-11-22 16:23:25 +0530

    firstBuild

---
+++ precache-manifest.287f3587c70f914ec6fb15a4a9256663.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ae045a2e86bbf89e224132938416516",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db3c7bb90954bfde5b9e",
+    "url": "/static/css/main.f83c5911.chunk.css"
+  },
+  {
+    "revision": "73d51313fb7d2cd7fd26",
+    "url": "/static/js/2.a00baaaa.chunk.js"
+  },
+  {
+    "revision": "3a24589c4bb9995cbc50abf564c6ffde",
+    "url": "/static/js/2.a00baaaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db3c7bb90954bfde5b9e",
+    "url": "/static/js/main.75b76481.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
